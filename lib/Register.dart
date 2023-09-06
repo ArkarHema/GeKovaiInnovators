4b189b66c35047059c5eb5a48de16145e544431a
ArkarHema <hema.2101111@srec.ac.in>
2023-09-06 22:39:23 +0530

    Initial commit

---
+++ lib/Register.dart
@@ -0,0 +1,285 @@
+import 'package:flutter/material.dart';
+import 'package:gehealth_care/SplashScreen.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  TextEditingController nameController = TextEditingController();
+  String selectedTiming1 = '7:00 AM';
+  String selectedTiming2 = '12:00 PM';
+  String selectedTiming3 = '6:00 PM';
+  final List<String> timingOptionsMrng = [
+    '7:00 AM',
+    '8:00 AM',
+    '9:00 AM',
+    '10:00 AM',
+    '11:00 AM'
+  ];
+  final List<String> timingOptionsAfter = [
+    '12:00 PM',
+    '1:00 PM',
+    '2:00 PM',
+    '3:00 PM',
+    '4:00 PM'
+  ];
+  final List<String> timingOptionsNight = [
+    '6:00 PM',
+    '7:00 PM',
+    '8:00 PM',
+    '9:00 PM',
+    '10:00 PM'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.orange, Colors.yellow],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20.0), // Adjust the spacing as needed
+                const Text(
+                  'Update Preferences',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black, // Text color set to black
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: MediaQuery.of(context).size.width *
+                      0.9, // One-third of the screen width
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.4), // Decreased opacity
+                    borderRadius:
+                        BorderRadius.circular(12.0), // Rounded corners
+                  ),
+                  child: Card(
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(10.0), // Rounded corners
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextField(
+                            controller: nameController,
+                            style: const TextStyle(
+                              color: Colors.black, // Text color set to black
+                            ),
+                            decoration: const InputDecoration(
+                              labelText: 'Name',
+
+                              prefixIcon: Icon(Icons.person),
+                              border: OutlineInputBorder(), // Rounded border
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          const Text(
+                            'Preferred time to eat',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors
+                                  .orangeAccent, // Text color set to black
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                children: [
+                                  const Text(
+                                    'Breakfast',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors
+                                          .orangeAccent, // Text color set to black
+                                    ),
+                                  ),
+                                  const SizedBox(width: 16),
+                                  DropdownButton<String>(
+                                    value: selectedTiming1,
+                                    onChanged: (newValue) {
+                                      setState(() {
+                                        selectedTiming1 = newValue!;
+                                      });
+                                    },
+                                    items: timingOptionsMrng
+                                        .map<DropdownMenuItem<String>>(
+                                            (String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(
+                                          value,
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                    style: const TextStyle(
+                                        color: Colors.black, fontSize: 16),
+                                    icon: const Icon(
+                                      Icons.arrow_drop_down,
+                                      color: Colors.green,
+                                      size: 32,
+                                    ),
+                                    elevation: 4,
+                                    underline: Container(
+                                      height: 2,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Text(
+                                    'Lunch',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors
+                                          .orangeAccent, // Text color set to black
+                                    ),
+                                  ),
+                                  const SizedBox(width: 46),
+                                  DropdownButton<String>(
+                                    value: selectedTiming2,
+                                    onChanged: (newValue) {
+                                      setState(() {
+                                        selectedTiming2 = newValue!;
+                                      });
+                                    },
+                                    items: timingOptionsAfter
+                                        .map<DropdownMenuItem<String>>(
+                                            (String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(
+                                          value,
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                    style: const TextStyle(
+                                        color: Colors.black, fontSize: 16),
+                                    icon: const Icon(
+                                      Icons.arrow_drop_down,
+                                      color: Colors.green,
+                                      size: 32,
+                                    ),
+                                    elevation: 4,
+                                    underline: Container(
+                                      height: 2,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Text(
+                                    'Dinner',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors
+                                          .orangeAccent, // Text color set to black
+                                    ),
+                                  ),
+                                  const SizedBox(width: 41),
+                                  DropdownButton<String>(
+                                    value: selectedTiming3,
+                                    onChanged: (newValue) {
+                                      setState(() {
+                                        selectedTiming3 = newValue!;
+                                      });
+                                    },
+                                    items: timingOptionsNight
+                                        .map<DropdownMenuItem<String>>(
+                                            (String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(
+                                          value,
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                    style: const TextStyle(
+                                        color: Colors.black, fontSize: 16),
+                                    icon: const Icon(
+                                      Icons.arrow_drop_down,
+                                      color: Colors.green,
+                                      size: 32,
+                                    ),
+                                    elevation: 4,
+                                    underline: Container(
+                                      height: 2,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 20),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (content) =>
+                                          const splashScreen(),
+                                    ),
+                                  );
+                                },
+                                child: const Text('Submit'),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/otp.dart
@@ -0,0 +1,213 @@
+import 'dart:math';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_sms/flutter_sms.dart';
+import 'package:gehealth_care/Register.dart';
+
+// ignore: camel_case_types
+class otp extends StatefulWidget {
+  final String name;
+  final String mobileNumber;
+  const otp({required this.name, required this.mobileNumber});
+  @override
+  State<otp> createState() => _otpState();
+}
+
+// ignore: camel_case_types
+class _otpState extends State<otp> {
+  TextEditingController otpController = TextEditingController();
+  String otp = '';
+  int _resendTimeout = 30;
+  bool _isResendDisabled = true;
+  Timer? _timer;
+  String generateOTP() {
+    // Generate a random 6-digit OTP
+    final random = Random();
+    return (100000 + random.nextInt(900000)).toString();
+  }
+
+  void sendOTP() async {
+    // Generate a random OTP (you can use a proper OTP generation library)
+    otp = generateOTP();
+
+    try {
+      String message = 'Hello ${widget.name}, your OTP is: $otp';
+      List<String> recipents = [widget.mobileNumber];
+
+      String result = await sendSMS(message: message, recipients: recipents);
+      print(result);
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
+  void startResendTimer() {
+    _isResendDisabled = true;
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_resendTimeout > 0) {
+        setState(() {
+          _resendTimeout--;
+        });
+      } else {
+        // Timer completed, enable resend button
+        setState(() {
+          _isResendDisabled = false;
+        });
+        _timer?.cancel();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    sendOTP();
+    startResendTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.orange, Colors.yellow],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'OTP Verification for ${widget.name}',
+                  style: const TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: MediaQuery.of(context).size.width *
+                      0.9, // One-third of the screen width
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.4), // Decreased opacity
+                    borderRadius:
+                        BorderRadius.circular(12.0), // Rounded corners
+                  ),
+                  child: Card(
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(10.0), // Rounded corners
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: otpController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              labelText: 'Enter OTP',
+                              prefixIcon: Icon(Icons.message),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Implement OTP verification logic here
+                              String enteredOTP = otpController.text;
+                              if (enteredOTP == otp) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (content) => Register(),
+                                  ),
+                                );
+                                print('OTP is valid');
+                              } else {
+                                // Invalid OTP
+                                print('Invalid OTP');
+                              }
+                            },
+                            child: const Text('Verify OTP'),
+                          ),
+                          const SizedBox(height: 20),
+                          _isResendDisabled
+                              ? Text('Resend OTP in $_resendTimeout seconds',
+                                  style: const TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.orangeAccent,
+                                  ))
+                              : ElevatedButton(
+                                  onPressed: () {
+                                    sendOTP();
+                                    setState(() {
+                                      _resendTimeout = 30;
+                                    });
+                                    startResendTimer();
+                                  },
+                                  child: const Text('Resend OTP'),
+                                )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+// Widget _textFieldOTP({bool first, last}) {
+//   return Container(
+//     height: 85,
+//     child: AspectRatio(
+//       aspectRatio: 1.0,
+//       child: TextField(
+//         autofocus: true,
+//         onChanged: (value) {
+//           if (value.length == 1 && last == false) {
+//             FocusScope.of(context).nextFocus();
+//           }
+//           if (value.length == 0 && first == false) {
+//             FocusScope.of(context).previousFocus();
+//           }
+//         },
+//         showCursor: false,
+//         readOnly: false,
+//         textAlign: TextAlign.center,
+//         style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//         keyboardType: TextInputType.number,
+//         maxLength: 1,
+//         decoration: InputDecoration(
+//           counter: Offstage(),
+//           enabledBorder: OutlineInputBorder(
+//               borderSide: BorderSide(width: 2, color: Colors.black12),
+//               borderRadius: BorderRadius.circular(12)),
+//           focusedBorder: OutlineInputBorder(
+//               borderSide: BorderSide(width: 2, color: Colors.purple),
+//               borderRadius: BorderRadius.circular(12)),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+// }
